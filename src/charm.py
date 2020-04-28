9f2eb4a76deb6709bf5656462285ee726e2507fe
Laurent Sesquès <laurent.sesques@canonical.com>
2020-04-28 21:56:45 +0200

    fix OCIImage handling

--- src/charm.py
+++ src/charm.py
@@ -11,6 +11,8 @@ from ops.model import (  # NoQA: E402
     WaitingStatus,
 )
 
+from oci_image import OCIImageResource
+
 import logging  # NoQA: E402
 logger = logging.getLogger()
 
@@ -21,6 +23,9 @@ class MattermostK8sCharm(CharmBase):
 
     def __init__(self, framework, key):
         super().__init__(framework, key)
+        # get our mattermost_image from juju
+        # ie: juju deploy . --resource mattermost_image=mattermost:latest )
+        self.mattermost_image = OCIImageResource(self, 'mattermost_image')
         self.framework.observe(self.on.start, self.configure_pod)
         self.framework.observe(self.on.config_changed, self.configure_pod)
         self.framework.observe(self.on.upgrade_charm, self.configure_pod)
@@ -29,12 +34,14 @@ class MattermostK8sCharm(CharmBase):
         if not self.framework.model.unit.is_leader():
             self.model.unit.status = WaitingStatus('Not a leader')
             return
+
+        mattermost_image_details = self.mattermost_image.fetch()
         self.model.unit.status = MaintenanceStatus('Configuring pod')
         config = self.model.config
         self.model.pod.set_spec({
             'containers': [{
                 'name': self.framework.model.app.name,
-                'imageDetails': {"imagePath": config["mattermost_image"]},
+                'imageDetails': mattermost_image_details,
                 'ports': [{
                     'containerPort': int(self.framework.model.config['mattermost_port']),
                     'protocol': 'TCP',
